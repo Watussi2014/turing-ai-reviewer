08fef69ceed1f5d79f1cd260b29ffa82f6e5a856
Watussi2014 <128143742+Watussi2014@users.noreply.github.com>
2025-05-10 10:38:31 +0300

    Added project_description_id attribute

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "project_descriptions" {
-  name           = "ProjectDescriptions"
+  name           = "project_descriptions_db"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "project_description_id"
 
@@ -10,12 +10,23 @@ resource "aws_dynamodb_table" "project_descriptions" {
 }
 
 resource "aws_dynamodb_table" "projects" {
-  name           = "Projects"
+  name           = "projects_db"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "project_id"
 
   attribute {
     name = "project_id"
     type = "S"
+  }
+
+  attribute {
+    name = "project_description_id"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "project_descriptions_index"
+    hash_key           = "project_description_id"
+    projection_type    = "ALL"
   }
 }
